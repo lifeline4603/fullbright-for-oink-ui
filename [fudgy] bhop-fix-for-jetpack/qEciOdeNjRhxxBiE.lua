1cccad0650a9e7e13f3409d3a844709b5c6da66c
lifeline4603 <135634664+lifeline4603@users.noreply.github.com>
2023-06-10 17:39:02 +0200

    added comments
    easier to read ig lmao

--- [fudgy] bhop-fix-for-jetpack/qEciOdeNjRhxxBiE.lua
+++ [fudgy] bhop-fix-for-jetpack/qEciOdeNjRhxxBiE.lua
@@ -1,3 +1,5 @@
+-- havent tested this yet but in theory this should work
+
 oink.ui_context("qEciOdeNjRhxxBiE")
 
 oink.ui_checkbox("jetpack bhop fix")
@@ -6,12 +8,12 @@ oink.ui_checkbox("jetpack bhop fix")
 oink.event_remove("view_render_post", "bhopFixEH")
 oink.event_listen("view_render_post", "bhopFixEH", function()
     if oink.ui_get("qEciOdeNjRhxxBiE", "jetpack bhop fix") then
-        local Owner = self:GetOwner()
+        local Owner = self:GetOwner() 
         local currentlyequipped = Owner:GetNWBool("hasJetpack", false)
-        if currentlyequipped then
-            oink.config_set("misc.bhop", "false")
+        if currentlyequipped then -- might also have to specify the func "function SWEP:PrimaryAttack()" but we'll see :)
+            oink.config_set("misc.bhop", "false") -- so we dont spam jump if jetpack is on
         else
-            oink.config_set("misc.bhop", "true")
+            oink.config_set("misc.bhop", "true") -- so we spam jump if jetpack is off
         end
     end
 end)
